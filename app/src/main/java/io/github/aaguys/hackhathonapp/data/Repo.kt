7e2c76014f098da8cfb1c7cc072c1c3d4eb23a07
Леонид Сухинин <l.suhinin@corp.mail.ru>
2019-06-21 22:34:46 +0300

    add repo

---
+++ app/src/main/java/io/github/aaguys/hackhathonapp/data/Repo.kt
@@ -0,0 +1,24 @@
+package io.github.aaguys.hackhathonapp.data
+
+import androidx.lifecycle.LiveData
+import io.github.aaguys.hackhathonapp.common.Event
+import io.github.aaguys.hackhathonapp.common.Speaker
+
+class Repo {
+
+    val Schedule: LiveData<List<Event>>
+    val Favorites: LiveData<List<Event>>
+
+    fun updateSchedule() {
+    }
+
+    fun addToFavorites(event: Event) {
+
+    }
+
+    fun removeFromFavorites(event: Event) {
+
+    }
+
+
+}
