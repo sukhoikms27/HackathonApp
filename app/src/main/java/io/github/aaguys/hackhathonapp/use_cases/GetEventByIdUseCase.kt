f993386b239b248e060a0895382a30ad22249951
Aleksey Yakovlev <aleksey@yakovlev.spb.ru>
2019-06-22 11:45:50 +0300

    EventDetails3

--- app/src/main/java/io/github/aaguys/hackhathonapp/use_cases/GetEventByIdUseCase.kt
+++ app/src/main/java/io/github/aaguys/hackhathonapp/use_cases/GetEventByIdUseCase.kt
@@ -4,5 +4,4 @@ import io.github.aaguys.hackhathonapp.data.Repo
 
 class GetEventByIdUseCase(repo: Repo, id: String) {
     val event = repo.schedule.value?.find { it.id == id }
-    val events = repo.schedule.value
 }
