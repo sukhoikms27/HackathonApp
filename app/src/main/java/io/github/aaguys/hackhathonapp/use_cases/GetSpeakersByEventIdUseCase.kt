b1c6de7c18e453fe297195ebfdf8869376bfd24f
Aleksey Yakovlev <aleksey@yakovlev.spb.ru>
2019-06-22 08:26:10 +0300

    useCases2

--- app/src/main/java/io/github/aaguys/hackhathonapp/use_cases/GetSpeakersByEventIdUseCase.kt
+++ app/src/main/java/io/github/aaguys/hackhathonapp/use_cases/GetSpeakersByEventIdUseCase.kt
@@ -3,5 +3,5 @@ package io.github.aaguys.hackhathonapp.use_cases
 import io.github.aaguys.hackhathonapp.data.Repo
 
 class GetSpeakersByEventIdUseCase(val repo: Repo, id :String) {
-    val event = repo.schedule.value?.filter { it.id == id }?.map{it.speakers}
+    val speaker = repo.schedule.value?.filter { it.id == id }?.map{it.speakers}
 }
